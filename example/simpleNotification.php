9ddd6b37b8f20cc73d70b9c92c7123b402750e1c
Zandor Smith <info@zsinfo.nl>
2019-04-26 16:55:18 +0000

    Added example.

---
+++ example/simpleNotification.php
@@ -0,0 +1,32 @@
+<?php
+
+use APNSFramework;
+
+require_once "vendor/autoload.php";
+
+try {
+    // Create APNS object
+    $apns = new APNS("teamId", "bundleId", "authKeyUrl", "authKeyId");
+
+    // Create notification
+    $notification = new APNSNotification();
+
+    $notification->setTitle("Example Notification");
+    $notification->setBody("This is an example notification!");
+
+    $notification->setBadge(2);
+
+    // Create token object
+    $tokenString = "MY_DEVICE_APNS_TOKEN";
+    $environment = APNSTokenEnvironment::development;
+    //$environment = APNSTokenEnvironment::production;
+    $token = new APNSToken($tokenString, $environment);
+
+    // Send the notification
+    $apns->sendNotification($notification, $token);
+
+    echo "Notification sent."
+} catch (APNSException $e) {
+    echo "Error: " . $e->getMessage();
+    // Handle exception
+}
