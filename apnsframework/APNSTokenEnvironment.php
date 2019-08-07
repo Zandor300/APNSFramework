63e3a7cbccb1dc0fe0d497222574e40af5d560cb
Zandor Smith <info@zsinfo.nl>
2019-08-07 06:19:40 +0000

    Add documentation url.

--- apnsframework/APNSTokenEnvironment.php
+++ apnsframework/APNSTokenEnvironment.php
@@ -2,6 +2,7 @@
 
 namespace APNSFramework;
 
+// See https://developer.apple.com/documentation/usernotifications/setting_up_a_remote_notification_server/sending_notification_requests_to_apns
 class APNSTokenEnvironment {
 
 	public const development = "https://api.sandbox.push.apple.com/3/device";
