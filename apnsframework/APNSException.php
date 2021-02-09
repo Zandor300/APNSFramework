c13c3a7862a446aee2d303ae13b9ec75cb5e6963
Zandor Smith <info@zsinfo.nl>
2021-02-09 13:37:31 +0100

    Move APNSException.

--- apnsframework/APNSException.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace APNSFramework;
-
-use Exception;
-
-class APNSException extends Exception {
-}
---
+++ apnsframework/exception/APNSException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace APNSFramework;
+
+use Exception;
+
+/**
+ * Class APNSException
+ * Exception used by APNSFramework for all exceptions. Other exceptions in the framework inherit this one.
+ * @package APNSFramework
+ */
+class APNSException extends Exception {
+}
