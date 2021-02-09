452feb56dfd76d232328436199095b3ac226abda
Zandor Smith <info@zsinfo.nl>
2021-02-09 13:45:53 +0100

    Replace all tabs with 4 spaces.

--- apnsframework/APNS.php
+++ apnsframework/APNS.php
@@ -10,59 +10,59 @@ use Firebase\JWT\JWT;
  */
 class APNS {
 
-	/**
-	 * @var string
-	 */
-	private $teamId;
+    /**
+     * @var string
+     */
+    private $teamId;
 
-	/**
-	 * @var string
-	 */
-	private $bundleId;
+    /**
+     * @var string
+     */
+    private $bundleId;
 
-	/**
-	 * @var string
-	 */
-	private $authKeyPath;
+    /**
+     * @var string
+     */
+    private $authKeyPath;
 
-	/**
-	 * @var string
-	 */
-	private $authKeyId;
+    /**
+     * @var string
+     */
+    private $authKeyId;
 
     /**
      * @var string|null
      */
     private $rootCertificatePath = null;
 
-	public function __construct($teamId, $bundleId, $authKeyPath, $authKeyId) {
-		$this->teamId = $teamId;
-		$this->bundleId = $bundleId;
-		$this->authKeyPath = $authKeyPath;
-		$this->authKeyId = $authKeyId;
-	}
+    public function __construct($teamId, $bundleId, $authKeyPath, $authKeyId) {
+        $this->teamId = $teamId;
+        $this->bundleId = $bundleId;
+        $this->authKeyPath = $authKeyPath;
+        $this->authKeyId = $authKeyId;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getTeamId(): string {
-		return $this->teamId;
-	}
+    /**
+     * @return string
+     */
+    public function getTeamId(): string {
+        return $this->teamId;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getBundleId(): string {
-		return $this->bundleId;
-	}
+    /**
+     * @return string
+     */
+    public function getBundleId(): string {
+        return $this->bundleId;
+    }
 
-	/**
-	 * @return string
+    /**
+     * @return string
      * @deprecated Use getAuthKeyPath() instead.
-	 */
-	public function getAuthKeyUrl(): string {
-		return $this->getAuthKeyPath();
-	}
+     */
+    public function getAuthKeyUrl(): string {
+        return $this->getAuthKeyPath();
+    }
 
     /**
      * @return string
@@ -71,12 +71,12 @@ class APNS {
         return $this->authKeyPath;
     }
 
-	/**
-	 * @return string
-	 */
-	public function getAuthKeyId(): string {
-		return $this->authKeyId;
-	}
+    /**
+     * @return string
+     */
+    public function getAuthKeyId(): string {
+        return $this->authKeyId;
+    }
 
     /**
      * Setter for setting the path to the root certificate .pem file.
@@ -85,8 +85,8 @@ class APNS {
      * Note: Might require an absolute path.
      * @param $rootCertificatePath string|null
      */
-	public function setRootCertificatePath($rootCertificatePath) {
-	    $this->rootCertificatePath = $rootCertificatePath;
+    public function setRootCertificatePath($rootCertificatePath) {
+        $this->rootCertificatePath = $rootCertificatePath;
     }
 
     /**
@@ -96,57 +96,57 @@ class APNS {
         return $this->rootCertificatePath;
     }
 
-	/**
-	 * Send the $notification to $token.
-	 * @param APNSNotification $notification The notification to send.
-	 * @param APNSToken $token The token to send the notification to.
-	 * @throws APNSException If an error occurred. See the message for more info.
+    /**
+     * Send the $notification to $token.
+     * @param APNSNotification $notification The notification to send.
+     * @param APNSToken $token The token to send the notification to.
+     * @throws APNSException If an error occurred. See the message for more info.
      * @throws APNSDeviceTokenInactive If the APNSToken is inactive and can be removed.
-	 */
-	public function sendNotification(APNSNotification $notification, APNSToken $token): void {
-		$authKey = file_get_contents($this->authKeyPath);
-		if($authKey == false) {
-			throw new APNSException("Can't read auth key. Failed to read file.");
-		}
+     */
+    public function sendNotification(APNSNotification $notification, APNSToken $token): void {
+        $authKey = file_get_contents($this->authKeyPath);
+        if ($authKey == false) {
+            throw new APNSException("Can't read auth key. Failed to read file.");
+        }
 
-		$authorization = JWT::encode(['iss' => $this->teamId, 'iat' => time()], $authKey, 'ES256', $this->authKeyId);
+        $authorization = JWT::encode(['iss' => $this->teamId, 'iat' => time()], $authKey, 'ES256', $this->authKeyId);
 
-		// Prepare the header.
-		$header = array();
-		$header[] = "content-type: application/json";
-		$header[] = "authorization: bearer {$authorization}";
-		$header[] = "apns-topic: {$this->bundleId}";
-		$header[] = "apns-push-type: " . ($notification->getBody() != null ? "alert" : "background");
-		$header[] = "apns-priority: " . $notification->getPriority();
+        // Prepare the header.
+        $header = array();
+        $header[] = "content-type: application/json";
+        $header[] = "authorization: bearer {$authorization}";
+        $header[] = "apns-topic: {$this->bundleId}";
+        $header[] = "apns-push-type: " . ($notification->getBody() != null ? "alert" : "background");
+        $header[] = "apns-priority: " . $notification->getPriority();
 
-		// Create the curl request.
-		$ch = curl_init($token->getTokenUrl());
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $notification->generateJSONPayload());
-		curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_2_0);
-		curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		if($this->rootCertificatePath != null) {
+        // Create the curl request.
+        $ch = curl_init($token->getTokenUrl());
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $notification->generateJSONPayload());
+        curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_2_0);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        if ($this->rootCertificatePath != null) {
             curl_setopt($ch, CURLOPT_CAINFO, $this->rootCertificatePath);
         }
 
-		// Execute the curl request.
-		$response = curl_exec($ch);
-		$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        // Execute the curl request.
+        $response = curl_exec($ch);
+        $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
-		$chError = curl_error($ch);
+        $chError = curl_error($ch);
         curl_close($ch);
-		if (!empty($chError)) {
-			throw new APNSException("curl error: $chError");
-		}
+        if (!empty($chError)) {
+            throw new APNSException("curl error: $chError");
+        }
 
-		if($httpcode == 400 || $httpcode == 403 || $httpcode == 404 || $httpcode == 405 || $httpcode == 413 || $httpcode == 429 || $httpcode == 500 || $httpcode == 503) {
+        if ($httpcode == 400 || $httpcode == 403 || $httpcode == 404 || $httpcode == 405 || $httpcode == 413 || $httpcode == 429 || $httpcode == 500 || $httpcode == 503) {
             $output = json_decode($response, true);
             throw new APNSException("APNs error: " . $output['reason'] . PHP_EOL . "See the following link for instructions: https://developer.apple.com/documentation/usernotifications/setting_up_a_remote_notification_server/handling_notification_responses_from_apns");
-        } else if($httpcode == 410) {
+        } else if ($httpcode == 410) {
             throw new APNSDeviceTokenInactive("The device token is inactive. It can be removed from the database until registered again. (" . $token->getToken() . ")");
-        } else if($httpcode != 200) {
+        } else if ($httpcode != 200) {
             throw new APNSException("APNs error: Unhandled http status code $httpcode." . PHP_EOL . "See the following link for instructions: https://developer.apple.com/documentation/usernotifications/setting_up_a_remote_notification_server/handling_notification_responses_from_apns");
         }
-	}
+    }
 
 }
--- apnsframework/APNSTokenEnvironment.php
+++ apnsframework/APNSTokenEnvironment.php
@@ -5,7 +5,7 @@ namespace APNSFramework;
 // See https://developer.apple.com/documentation/usernotifications/setting_up_a_remote_notification_server/sending_notification_requests_to_apns
 class APNSTokenEnvironment {
 
-	public const development = "https://api.sandbox.push.apple.com/3/device";
-	public const production = "https://api.push.apple.com/3/device";
+    public const development = "https://api.sandbox.push.apple.com/3/device";
+    public const production = "https://api.push.apple.com/3/device";
 
 }
